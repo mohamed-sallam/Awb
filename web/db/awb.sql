717cfd7cbfe3749f7861344c9f36c4a2ed482f0d
Mohamed-Sallam <muhd.sallam@gmail.com>
2021-04-27 15:39:44 +0200

    Editing on DB for security and privacy

--- web/db/awb.sql
+++ web/db/awb.sql
@@ -9,13 +9,3 @@ CREATE TABLE `users`(
 );
 ALTER TABLE
     `users` ADD PRIMARY KEY `users_id_primary`(`id`);
-CREATE TABLE `blocked_processes`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_id` INT UNSIGNED NOT NULL,
-    `device` VARCHAR(255) NOT NULL,
-    `process` VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    `blocked_processes` ADD PRIMARY KEY `blocked_processes_id_primary`(`id`);
-ALTER TABLE
-    `blocked_processes` ADD CONSTRAINT `blocked_processes_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
